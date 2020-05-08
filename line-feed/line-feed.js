4f77aaa3eb269a62db4e3327ca5c7046da911b89
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2020-05-08 10:26:40 -0400

    Update line-feed.js

--- line-feed/line-feed.js
+++ line-feed/line-feed.js
@@ -1,7 +1,5 @@
-function interpret() {
-  if (!/^(\r?\n)*$/.test(document.getElementById("code").value)) {
-    let output = document.getElementById("output");
-    output.style.color = "red";
-    output.value = "SyntaxError: Invalid character in source code";
+function interpret(code) {
+  if (!/^(\r?\n)*$/.test(code)) {
+    console.log("\033[38;2;255;0;0mSyntaxError: Invalid character in source code\033[0;0;0m");
   }
 }
