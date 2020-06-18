e760e7b6c2cd7e96b413f84ab66a5e119af30468
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2020-06-18 09:53:40 -0400

    Update ubyte.py

--- gorbitsa/ubyte.py
+++ gorbitsa/ubyte.py
@@ -1,6 +1,13 @@
 class UByte:
   def __init__(self, n):
-    self.n = n % 256
+    if isinstance(n, (int, float)):
+      self.n = (n - n % 1) % 256
+    elif isinstance(n, str):
+      self.n = int(n) % 256
+    elif isinstance(n, UByte):
+      self.n = n.n
+    else:
+      raise TypeError("cannot convert", type(n), "to UByte")
   
   def __iadd__(self, value):
     if isinstance(value, UByte):
@@ -32,6 +39,38 @@ class UByte:
     else:
       raise TypeError("cannot perform XOR on", type(value), "and UByte")
   
+  def __lt__(self, value):
+    if isinstance(value, UByte):
+      return self.n < value.n
+    elif isinstance(value, int):
+      return self.n < value
+    else:
+      raise TypeError("cannot compare", type(value), "with UByte")
+  
+  def __gt__(self, value):
+    if isinstance(value, UByte):
+      return self.n > value.n
+    elif isinstance(value, int):
+      return self.n > value
+    else:
+      raise TypeError("cannot compare", type(value), "with UByte")
+  
+  def __le__(self, value):
+    if isinstance(value, UByte):
+      return self.n <= value.n
+    elif isinstance(value, int):
+      return self.n <= value
+    else:
+      raise TypeError("cannot compare", type(value), "with UByte")
+  
+  def __ge__(self, value):
+    if isinstance(value, UByte):
+      return self.n >= value.n
+    elif isinstance(value, int):
+      return self.n >= value
+    else:
+      raise TypeError("cannot compare", type(value), "with UByte")
+  
   def __eq__(self, value):
     if isinstance(value, UByte):
       return self.n == value.n
@@ -39,6 +78,15 @@ class UByte:
       return self.n == value
     else:
       raise TypeError("cannot compare", type(value), "with UByte")
+  
+  def __ne__(self, value):
+    if isinstance(value, UByte):
+      return self.n != value.n
+    elif isinstance(value, int):
+      return self.n != value
+    else:
+      raise TypeError("cannot compare", type(value), "with UByte")
+  
   def __int__(self):
     return self.n
 
