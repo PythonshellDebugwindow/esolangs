0839d54656e4d1bf99ec983218f3f1a5e6361163
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2020-06-24 11:04:58 -0400

    Update plus-or-minus.h

--- plus-or-minus/plus-or-minus.h
+++ plus-or-minus/plus-or-minus.h
@@ -2,16 +2,17 @@
 
 void plusOrMinus(const char *code)
 {
-  int acc = 0;
+  short acc = 0;
   unsigned long i;
   for(i = 0; code[i]; ++i)
   {
     if(code[i] == '+' && ++acc > 255)
       acc = 0;
-    if(code[i] == '-')
+    else if(code[i] == '-')
     {
       printf("%c", (char) acc);
-      if(--acc < 0) acc = 255;
+      if(--acc < 0)
+        acc = 255;
     }
   }
   printf("\n\033[32;1m=> %d\033[0m\n", acc);
