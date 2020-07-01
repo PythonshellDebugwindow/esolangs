b0778ee5e4060a4697f5b6159e1f64f648245fd3
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2020-07-01 09:33:10 -0400

    Update blues-machine.py

--- blues-machine/blues-machine.py
+++ blues-machine/blues-machine.py
@@ -5,7 +5,7 @@ def blues_machine(code):
   mem = defaultdict(lambda: 0)
   i = 0
   while 0 <= i < clen:
-    if code[i][0] != "" and code[i][0] == "blues":
+    if code[i][0] == "blues":
       a,b,c,d,e,f,g,h = map(int, code[i][1:])
       mem[a] += 1
       mem[b] *= mem[c]
@@ -15,8 +15,7 @@ def blues_machine(code):
         mem[d] = 0
       if mem[f] != 0:
         i = (h if g else mem[h])
-      else:
-        i += 1
-      print(dict(mem.items()))
-    elif code[i][0][0] != "#":
+        continue
+    elif code[i][0] != "" and code[i][0][0] != "#":
       raise SyntaxError("blues machine: invalid syntax")
+    i += 1
