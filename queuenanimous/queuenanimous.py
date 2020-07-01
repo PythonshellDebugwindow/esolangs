f91d75d5b47eeeb47d3865057192bd42e21c9ef5
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2020-07-01 14:51:50 -0400

    Update queuenanimous.py

--- queuenanimous/queuenanimous.py
+++ queuenanimous/queuenanimous.py
@@ -13,12 +13,19 @@ def queuenanimous(code):
   q = []
   i = 0
   b = brkmap(code)
-  rb = {v: k for k, v in b}
+  rb = {v: k for k, v in b.items()}
   while 0 <= i < clen:
-    if code[i] == "0": q.append(0)
-    elif code[i] == "+": q.append(q.pop(0) + 1)
-    elif code[i] == "-": q.append(q.pop(0) - 1)
-    elif code[i] == ">": q.append(q.pop(0))
-    elif code[i] == "[" and q.pop(0) == 0: i = b[i]
-    elif code[i] == "]": i = rb[i]
+    if code[i] == "0":
+      q.append(0)
+    elif code[i] == "+":
+      q.append((q or [0]).pop(0) + 1)
+    elif code[i] == "-":
+      q.append((q or [0]).pop(0) - 1)
+    elif code[i] == ">":
+      q.append((q or [0]).pop(0))
+    elif code[i] == "[":
+      if (q or [0]).pop(0) == 0:
+        i = b[i]
+    elif code[i] == "]":
+      i = rb[i] - 1
     i += 1
