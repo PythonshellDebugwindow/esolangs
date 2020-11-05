68a95543613e7cb701bdfd8472d9fb0b3ae59f45
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2020-11-05 17:03:39 -0500

    Make the file actually run

--- n-refine/n-refine.py
+++ n-refine/n-refine.py
@@ -1,7 +1,11 @@
-import random
+import sys, random
 
 def n_refine(code, debug=True):
-  code = list(map(int, code.split()))
+  try:
+    code = list(map(int, code.split()))
+  except ValueError:
+    print("n-refine.py: Error: invalid syntax")
+    sys.exit(1)
   numbers = {}
   i = 0
   while 0 <= i < len(code):
@@ -17,3 +21,26 @@ def n_refine(code, debug=True):
     else:
       numbers[b] = a
     i += 2
+
+if __name__ == "__main__":
+  if len(sys.argv) <= 1:
+    print("Usage: n-refine.py [OPTION] FILE")
+    print("Type 'n-refine.py --help' for more information")
+  elif "--help" in sys.argv:
+    print("""Usage: n-refine.py [OPTION] FILE
+FILE must not start with a dash.
+Options:
+    -d, --debug  Use debug mode (prints the current instruction before executing)
+    --help       Print this message
+    -i, --input  Get the code from STDIN instead of reading from a file""")
+  else:
+    code = ""
+    sys.argv.pop(0)
+    if "-i" in sys.argv or "--input" in sys.argv:
+      code = input("Enter code:")
+    elif any(map(lambda a: a[0] != "-", sys.argv)):
+      code = open(tuple(filter(lambda a: a[0] != "-", sys.argv))[0]).read()
+    else:
+      print("n-refine.py: Error: no FILE provided")
+      sys.exit(1)
+    n_refine(code, "-d" in sys.argv or "--debug" in sys.argv)
